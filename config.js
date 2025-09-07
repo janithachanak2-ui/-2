8390fcb2ff0e7d036564170c4319e172ddf42b78
janitha <janithachanak2@gmail.com>
2025-09-07 11:36:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/qqxfbh.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*𝐉𝐀𝐍𝐈𝐘𝐀 𝐕𝟏𝟎 THAMA PANAPITIN HUTTO😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
